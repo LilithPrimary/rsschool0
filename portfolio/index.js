b5e28b7af2e3f48d8315905802efdd786831fc60
Denna <lilithprimary@gmail.com>
2022-01-09 22:19:01 +0300

    feat: add buttons and language switch

---
+++ portfolio/index.js
@@ -0,0 +1,9 @@
+let btnContainer = document.getElementById("pfBtns");
+let btns = btnContainer.getElementsByClassName("button");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[1].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
