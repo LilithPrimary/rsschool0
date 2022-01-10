6c57665168d254603cdb443461c512beb4d7dd82
Denna <lilithprimary@gmail.com>
2022-01-10 15:02:50 +0300

    feat: finish portfolio#1

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,7 +3,7 @@ let btns = btnContainer.getElementsByClassName("button");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     let current = document.getElementsByClassName("active");
-    current[1].className = current[0].className.replace(" active", "");
+    current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
