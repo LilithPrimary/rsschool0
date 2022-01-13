59e329991123dc6df80ca4f2d1fa4981b1259b88
Denna <lilithprimary@gmail.com>
2022-01-13 19:55:48 +0300

    feat: cool burger button

--- portfolio/index.js
+++ portfolio/index.js
@@ -2,16 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const main = document.querySelector(".main");
 const menuLinks = document.querySelectorAll(".menu-link");
-const blockInstedBurger = document.querySelector(".instedBurger");
 function toggleMenu() {
   hamburger.classList.toggle("open");
   nav.classList.toggle("open");
-  blockInstedBurger.classList.toggle("open");
 }
 function closeMenu() {
   nav.classList.remove("open");
   hamburger.classList.remove("open");
-  blockInstedBurger.classList.remove("open");
 }
 hamburger.addEventListener("click", toggleMenu);
 menuLinks.forEach(el => el.addEventListener("click", closeMenu));
