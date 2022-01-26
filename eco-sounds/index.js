386b98d540ed45b9bb5b848d5037d651ed4f8860
Denna <lilithprimary@gmail.com>
2022-01-26 19:26:13 +0300

    feat: add images change

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,1 +1,20 @@
+const img = document.querySelector(".main__image");
+const menuLinks = document.querySelectorAll("[data-bird]");
 
+menuLinks.forEach(el => el.addEventListener("click", (e) =>{
+    let bird = e.target.dataset.bird;
+    let current = document.querySelector(".is_active");
+    if (e.target.dataset.bird !== current.dataset.bird) {
+        current.classList.remove("is_active")
+        e.target.classList.add("is_active");
+        changeBird(bird);
+    }
+}))
+
+function changeBird (bird) {
+    img.classList.add("hidden");
+    setTimeout(() => {
+        img.src = `assets/img/${bird}.jpg`;
+        img.classList.remove("hidden");
+    }, 300);
+}
