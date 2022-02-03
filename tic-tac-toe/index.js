2145cc4184822d793d591e1bb79a132d1cf1fc3a
Denna <lilithprimary@gmail.com>
2022-02-03 12:02:07 +0300

    feat: add music and sounds

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -5,13 +5,24 @@ const title = document.querySelector(".text");
 const button = document.querySelector(".button");
 const crossLine = document.querySelector(".cross-line");
 const headerTitle = document.querySelector(".title");
-let array = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+const zeroSound = document.querySelector(".zero-sound");
+const crossSound = document.querySelector(".cross-sound");
+const finishSound = document.querySelector(".finish-sound");
+const music = document.querySelector(".bg-sound");
+const playBtn = document.querySelector(".button__play");
+music.volume = 0.1;
+music.loop = true;
+zeroSound.volume = 0.2;
+crossSound.volume = 0.2;
+finishSound.volume = 0.2;
+let isPlay = false;
+let win = false;
 let counter = 0;
-const zero = "assets/svg/zero.svg";
-const cross = "assets/svg/cross.svg";
+const zero = "./assets/svg/zero.svg";
+const cross = "./assets/svg/cross.svg";
 
 grid.addEventListener ("click", e => {
-    if (counter < 9) {
+    if (counter < 9 && !win) {
         if (e.target.classList.contains("cell")) {
             addSign(e.target);
             checkWinner();
@@ -24,6 +35,7 @@ function addSign(element) {
         const img = document.createElement("img");
         img.classList.add("sign");
         img.src = counter % 2 === 0 ? cross : zero;
+        counter % 2 === 0 ? crossSound.play() : zeroSound.play();
         element.id = counter % 2 === 0 ? "cross" : "zero";
         element.append(img);
         counter++;
@@ -43,9 +55,9 @@ function checkWinner() {
     ];
     for (let el of winCombination) {
         if (cells[el[0]].id === "cross" && cells[el[1]].id === "cross" && cells[el[2]].id === "cross") {
-            showResult("Crosses wins", el[3]); return;
+            win = true; showResult("Crosses wins", el[3]); return;
         } else if (cells[el[0]].id === "zero" && cells[el[1]].id === "zero" && cells[el[2]].id === "zero") {
-            showResult("Zeros wins", el[3]); return;
+            win = true; showResult("Zeros wins", el[3]); return;
         }
     }
     if (counter === 9) {
@@ -59,6 +71,7 @@ function showResult(result, line) {
     }
     title.textContent = result;
     setTimeout(() => {
+        finishSound.play();
         grid.classList.add("hide");
         textCont.classList.remove("hide");
         if (line != "") {
@@ -69,6 +82,7 @@ function showResult(result, line) {
 
 button.addEventListener("click", () => newGame());
 headerTitle.addEventListener("click", () => newGame());
+playBtn.addEventListener("click", () => playPause());
 
 function newGame() {
     cells.forEach(el => {
@@ -76,6 +90,19 @@ function newGame() {
         el.id = "null";
     });
     counter = 0;
+    win = false;
     grid.classList.remove("hide");
     textCont.classList.add("hide");
+}
+
+function playPause() {
+    if (isPlay) {
+        music.pause();
+        isPlay = false;
+        playBtn.firstElementChild.href.baseVal = "./assets/svg/sprite.svg#play"
+    } else {
+        music.play();
+        isPlay = true;
+        playBtn.firstElementChild.href.baseVal = "./assets/svg/sprite.svg#pause"
+    }
 }
